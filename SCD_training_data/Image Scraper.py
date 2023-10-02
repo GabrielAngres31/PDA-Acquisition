db447446382e900bd9bace4e083dfbee6e76a814
Gabriel Angres <gangres@nevada.unr.edu>
2023-10-02 16:24:26 -0700

    Updated code to process multiple files

--- SCD_training_data/Image Scraper.py
+++ SCD_training_data/Image Scraper.py
@@ -8,8 +8,10 @@ from tkinter import filedialog as fd
 from PIL import Image, ImageTk
 import time
 
-DIR_CWD = os.path.join(os.getcwd(), "SCD_training_data")
+DIR_CWD = os.getcwd()
 DIR_SOURCE = os.path.join(DIR_CWD, "source_images")
+DIR_BASE = os.path.join(DIR_SOURCE, "BASE")
+DIR_ANNO = os.path.join(DIR_SOURCE, "ANNOTATION")
 
 def timer_func(func):
     def wrap_func(*args, **kwargs):
@@ -20,37 +22,65 @@ def timer_func(func):
         return result
     return wrap_func
 
-SEARCH_FILE = imread("C:\\Users\\gjang\\Documents\\GitHub\\PDA-Acquisition\\SCD_training_data\\source_images\\BASE\\cot1.tif")
-MASK_FILE = imread("C:\\Users\\gjang\\Documents\\GitHub\\PDA-Acquisition\\SCD_training_data\\source_images\\ANNOTATION\\cot1_STOMATA_MASKS.tiff")
-
-print(SEARCH_FILE.shape)
-height, width = SEARCH_FILE.shape[0], SEARCH_FILE.shape[1]
-
-segment_size = 64
+#SEARCH_FILE = imread(os.path.join(DIR_SOURCE, "BASE\\cot1.tif"))
+#MASK_FILE = imread(os.path.join(DIR_SOURCE, "ANNOTATION\\cot1_STOMATA_MASKS.tiff"))
 
 @timer_func
-def timeTaker(print_it = False):
-    global SEARCH_FILE
-    global MASK_FILE
+def timeTaker(searchfile, maskedfile, print_it = False):
+    SEARCH_FILE = imread(searchfile)
+    MASKED_FILE = imread(maskedfile)
+   
+    height, width = SEARCH_FILE.shape[0], SEARCH_FILE.shape[1]
+    segment_size = 64
+    trial = 0
     section_counts = [0,0,0]
-    print(list(range(width))[-1])
-    print(list(range(width))[0])
-    for x in list(range(width)):
-        for y in list(range(height)):
+    sample_interval = 8
+   
+    for x in list(range(0, width, sample_interval)):
+        for y in list(range(0, height, sample_interval)):
+            if trial == 80:
+                break
+
             chunk = SEARCH_FILE[y:y+segment_size,
                                 x:x+segment_size]
+            #print(chunk.size)
             if chunk.size != 4096:
                 break
             #print(chunk.size)
-            if np.all(chunk[0, :] == 0) and np.all(chunk[:, 0] == 0) and np.all(chunk[:, segment_size-1] == 0) and np.all(chunk[segment_size-1, :] == 0):
-                pass
-            else:
-                if np.mean(MASK_FILE[y:y+segment_size, x:x+segment_size]) > 0:
-                    #section_counts[2] += 1
-                    imwrite(f"C:\\Users\\gjang\\Documents\\GitHub\\PDA-Acquisition\\SCD_training_data\\source_images\\generated\\test\\COT1 - {x}x - {y}y.tif", chunk)
+            #if np.all(chunk[0, :] == 0) and np.all(chunk[:, 0] == 0) and np.all(chunk[:, segment_size-1] == 0) and np.all(chunk[segment_size-1, :] == 0):
+            #    pass
+            #else:
+            chunk_mask = MASKED_FILE[y:y+segment_size,
+                                     x:x+segment_size]
+            label_string = ""
 
-                            
+            # TODO: TROUBLESHOOT THIS!!!
+            if np.all(chunk_mask == 0): # If no annotation is present in the cell
+                if np.all(chunk < 12): # If the cell is mostly or all black, hop out of the loop
+                    break
+                else: # Else, mark it as ABSENT (plant part that has no stomatal features)
+                    label_string = "ABSENT"
+            elif np.all(chunk_mask[0, :] == 0) and np.all(chunk_mask[:, 0] == 0) and np.all(chunk_mask[:, segment_size-1] == 0) and np.all(chunk_mask[segment_size-1, :] == 0):
+                # If annotation is present in the cell and there's no annotation content on the cell borders, then a stomata is wholly enclosed
+                label_string = "WHOLE"
+            else: # Otherwise it is not.
+                label_string = "PARTIAL"
+            imwrite(os.path.join(DIR_SOURCE, f"generated\\test\\{label_string}\\COT1_{label_string}_({x}, {y}).tif"), chunk)
+            trial += 1
+            if trial % 40000 == 0:
+                print(f"{trial/1000}k images stored!")
+            #print(f"Loop #{trial}")
+
+
+    print(f"{trial} images stored!")
     print(f'Empty Sections:{section_counts[0]}\nNon-Empty Sections:{section_counts[1]}\nAnnotated Sections:{section_counts[2]}')
 
+def file_iterator():
 
-timeTaker(print_it = True)
+    for i in list(range(1, 5)):
+        searchfile_name = os.path.join(DIR_BASE, f"cot{i}.tif")
+        maskedfile_name = os.path.join(DIR_ANNO, f"cot{i}_STOMATA_MASKS.tiff")
+        timeTaker(searchfile_name, maskedfile_name, print_it = True)
+    print("There was an attempt!")
+
+file_iterator()
